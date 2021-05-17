bf14f13b2ca2f9b7033e18b622db661b68c12a1f
Owen Keary <owenkeary@hotmail.co.uk>
2021-05-17 20:09:15 +0100

    update to api call

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,6 +3,12 @@
 export default (req, res) => {
   res.statusCode = 200
 
+  let data = getOpenMapsData();
+
+  res.json(data);
+}
+
+async function getOpenMapsData() {
   let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + req['headers']['x-vercel-ip-city'] + "&appid=" + process.env.OpenWeatherAPIKey
   const res = await fetch(openWeatherURL)
   const data = await res.json()
@@ -12,21 +18,20 @@ export default (req, res) => {
       notFound: true,
     }
   }
-  res.json(
-      { 
-        client: req['headers'],
-        timestamp: {
-          date: "17/05/2021",
-          time: "19:15"
-        },
-        weather: data["weather"],
-        base: data["base"],
-        main: data["main"],
-        visibility: data["visibility"],
-        wind: data["wind"],
-        clouds: data["clouds"],
-        dt: data["dt"],
-        sys: data["sys"]
-      }
-    )
+
+  return { 
+      client: req['headers'],
+      timestamp: {
+        date: "17/05/2021",
+        time: "19:15"
+      },
+      weather: data["weather"],
+      base: data["base"],
+      main: data["main"],
+      visibility: data["visibility"],
+      wind: data["wind"],
+      clouds: data["clouds"],
+      dt: data["dt"],
+      sys: data["sys"]
+    };
 }
