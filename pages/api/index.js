5d7ff09dbaf660668d4272cefad17226d8205437
Owen Keary <owenkeary@hotmail.co.uk>
2021-05-17 20:11:21 +0100

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,36 +2,49 @@
 
 export default (req, res) => {
   res.statusCode = 200
-
-  let data = getOpenMapsData();
-
-  res.json(data);
-}
-
-async function getOpenMapsData() {
-  let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + req['headers']['x-vercel-ip-city'] + "&appid=" + process.env.OpenWeatherAPIKey
-  const res = await fetch(openWeatherURL)
-  const data = await res.json()
-
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
-
-  return { 
-      client: req['headers'],
-      timestamp: {
-        date: "17/05/2021",
-        time: "19:15"
-      },
-      weather: data["weather"],
-      base: data["base"],
-      main: data["main"],
-      visibility: data["visibility"],
-      wind: data["wind"],
-      clouds: data["clouds"],
-      dt: data["dt"],
-      sys: data["sys"]
-    };
+  res.json(
+      { 
+        client: {
+          ip: req["x-forwarded-for"],
+          city: req["x-vercel-ip-city"],
+          country: req["x-vercel-ip-country"]
+        },
+        timestamp: {
+          date: "17/05/2021",
+          time: "19:15"
+        },
+        weather: {
+          id: 800,
+          main: "Clear",
+          description: "clear sky",
+          icon: "01d"
+        },
+        base: "stations",
+        main: {
+          temp: 282.55,
+          feels_like: 281.86,
+          temp_min: 280.37,
+          temp_max: 284.26,
+          pressure: 1023,
+          humidity: 100
+        },
+        visibility: 16093,
+        wind: {
+          speed: 1.5,
+          deg: 350
+        },
+        clouds: {
+          all: 1
+        },
+        dt: 1560350645,
+        sys: {
+          type: 1,
+          id: 5122,
+          message: 0.0139,
+          country: "US",
+          sunrise: 1560343627,
+          sunset: 1560396563
+        },
+      }
+    )
 }
